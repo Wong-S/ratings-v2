8552b458d96da083d7e28a5478f5b3e72921fb76
Sarah Wong <swong3310@gmail.com>
2020-10-28 20:51:08 -0700

    Add class for database

--- model.py
+++ model.py
@@ -1,25 +1,73 @@
 """Models for movie ratings app."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 # Replace this with your code!
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(10))
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f"<User user_id = {self.user_id} email = {self.email}>"
 
 
-def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    flask_app.config['SQLALCHEMY_ECHO'] = echo
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.app = flask_app
     db.init_app(flask_app)
 
-    print('Connected to the db!')
+    print("Connected to the db!")
 
 
-if __name__ == '__main__':
+# Creating Movie Class
+class Movie(db.Model):
+    """A movie."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    # ratings = a list of Rating objects
+
+    def __repr__(self):
+        return f"<Movie movie_id = {self.movie_id} title = {self.title}>"
+
+
+class Rating(db.Model):
+    """A rating."""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings")
+
+    def __repr__(self):
+        return f"<Rating rating_id={self.rating_id} score={self.score}>"
+
+
+if __name__ == "__main__":
     from server import app
 
     # Call connect_to_db(app, echo=False) if your program output gets
